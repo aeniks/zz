89c2415ab1d0f6689d782d47e802357ae896f6c5
u0_a79 <u0_a79@localhost>
2025-09-16 19:03:11 +0200

    __Tue Sep 16 19:03:11 CEST 2025

--- zmenu.sh
+++ zmenu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ## menu fzf
-zmenu() { local IFS=$'\n\t '; alts=($*); [ -z $* ] && alts=($(command ls -pw1 ./)); allw=($(printf %b "${alts[*]}"|wc -l --max-line-length)); printf %b "${alts[*]}"|bat -ppfljava|fzf-tmux -h $((allw + 4)) -w $((allw[1] + 6)) --ansi --bind "q:abort" --no-input --color "gutter:#000000,bg:#121212,bg+:#424242,list-bg:#222222,border:#424288" --no-sort; }; 
+zmenu() { local IFS=$'\n\t '; alts=($*); [ -z "$1" ] && alts=($(command ls -pw1 ./)); allw=($(printf %b "${alts[*]}"|wc -l --max-line-length|tr -s "\n\t " " ")); printf %b "${alts[*]}"|bat -ppfld --theme Dracula|fzf-tmux --min-height 2 -h $((allw + 4)) -w $((allw[1] + 6)) --ansi --bind "q:abort" --no-input --color "gutter:#000000,bg:#121212,bg+:#424242,list-bg:#222222,border:#424288" --no-sort; }; 
